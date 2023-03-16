76b0be540636eda41120f2bbad4f0f16b6474778
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-16 17:07:25 +0530

    Added:code to enter a valid last name

--- UserValidations.java
+++ UserValidations.java
@@ -6,10 +6,13 @@ public class UserValidations {
     static Scanner scanner = new Scanner(System.in);
     public static void UserName() {
         System.out.println("Enter First Name");
-        Pattern pattern = Pattern.compile("^[A-Z]{1}[A-Za-z]{2,}");
+        Pattern pattern = Pattern.compile("^[A-Z]{1}[A-Za-z]{2,}$");
         String firstName = scanner.next();
         Matcher matcher = pattern.matcher(firstName);
+        String lastName = scanner.next();
+        Matcher matcher1 = pattern.matcher(lastName);
         if(matcher.matches()){
+            if (matcher1.matches())
             System.out.println("Valid Name");
         }else
             System.out.println("Invalid Name");
