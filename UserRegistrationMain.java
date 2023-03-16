7ba730416aead7bb50c8346500e9654f8de038fc
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-16 18:16:47 +0530

    Added:code to enter a valid email id

--- UserRegistrationMain.java
+++ UserRegistrationMain.java
@@ -2,6 +2,7 @@ public class UserRegistrationMain {
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Problem");
         UserValidations userValidations = new UserValidations();
-        userValidations.UserName();
+        userValidations.username();
+        userValidations.useremail();
     }
 }
