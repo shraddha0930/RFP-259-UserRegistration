8c84bf2186d4766d16884dc75e0969c5913649e1
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-16 09:24:04 +0530

    Added:code to enter a valid first name

--- UserRegistrationMain.java
+++ UserRegistrationMain.java
@@ -1,5 +1,7 @@
 public class UserRegistrationMain {
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Problem");
+        UserValidations userValidations = new UserValidations();
+        userValidations.UserName();
     }
 }
