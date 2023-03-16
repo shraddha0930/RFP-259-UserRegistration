9e3edb29a0ef4405e8b4b43fb125e7325ea0634d
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-16 18:16:04 +0530

    Added:code to enter a valid email id

--- UserValidations.java
+++ UserValidations.java
@@ -4,16 +4,28 @@ import java.util.regex.Pattern;
 
 public class UserValidations {
     static Scanner scanner = new Scanner(System.in);
-    public static void UserName() {
+    public void username() {
         System.out.println("Enter First Name");
         Pattern pattern = Pattern.compile("^[A-Z]{1}[A-Za-z]{2,}$");
         String firstName = scanner.next();
         Matcher matcher = pattern.matcher(firstName);
+        System.out.println("Enter Last Name");
         String lastName = scanner.next();
         Matcher matcher1 = pattern.matcher(lastName);
         if(matcher.matches()){
             if (matcher1.matches())
             System.out.println("Valid Name");
+        }else
+            System.out.println("Invalid Name");
+
+    }
+    public void useremail() {
+        System.out.println("Enter EmailId");
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+([.][a-zA-Z0-9]*)?[@][a-zA-Z0-9]+[.][a-z]{2,}([.][a-zA-Z]{2,})?$");
+        String email = scanner.next();
+        Matcher matcher = pattern.matcher(email);
+        if(matcher.matches()){
+                System.out.println("Valid Name");
         }else
             System.out.println("Invalid Name");
 
