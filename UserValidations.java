87bc4800daf3c92c61c537c6a2d1b0f8cfa68945
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-17 19:41:27 +0530

    Refactor:code to check all email sample provided seperately

--- UserValidations.java
+++ UserValidations.java
@@ -3,7 +3,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserValidations {
-    static Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
+
     public void username() {
         System.out.println("Enter First Name");
         Pattern pattern = Pattern.compile("^[A-Z]{1}[A-Za-z]{2,}$");
@@ -12,43 +13,51 @@ public class UserValidations {
         System.out.println("Enter Last Name");
         String lastName = scanner.next();
         Matcher matcher1 = pattern.matcher(lastName);
-        if(matcher.matches()){
+        if (matcher.matches()) {
             if (matcher1.matches())
-            System.out.println("Valid Name");
-        }else
+                System.out.println("Valid Name");
+        } else
             System.out.println("Invalid Name");
 
     }
-    public void useremail() {
-        System.out.println("Enter EmailId");
-        Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+([.][a-zA-Z0-9]*)?[@][a-zA-Z0-9]+[.][a-z]{2,}([.][a-zA-Z]{2,})?$");
-        String email = scanner.next();
-        Matcher matcher = pattern.matcher(email);
-        if(matcher.matches()){
-                System.out.println("Valid EmailId");
-        }else
-            System.out.println("Invalid EmailId");
 
+    public void useremail() {
+        String[] validemails = {"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com",
+                "abc11@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com",
+                "abc@gmail.com.com", "abc+100@gmail.com"};
+        String[] invalidemails = {"abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com",
+                "abc123@gmail.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com",
+                "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au",};
+        for (String emails : validemails) {
+            Pattern.matches("^[a-z+.]+@(.+)$",emails);
+            System.out.println("Valid EmailId:");
+        }
+        for (String email : invalidemails) {
+            Pattern.matches("^[a-z+.]+@(.+)$",email);
+            System.out.println("Invalid EmailId");
+        }
     }
+
     public void mobilenumber() {
         System.out.println("Enter Mobile Number");
         Pattern pattern = Pattern.compile("^[1-9]{2}(\\s)[6-9]{1}[0-9]{9}");
         String mobile = scanner.next();
         Matcher matcher = pattern.matcher(mobile);
-        if(matcher.matches()){
+        if (matcher.matches()) {
             System.out.println("Valid Mobile number");
-        }else
+        } else
             System.out.println("Invalid Mobile number");
 
     }
+
     public void userpassword() {
         System.out.println("Enter password");
         Pattern pattern = Pattern.compile("[A-Za-z0-9](?=.*[-+_!@#$%^&*.,?]{1}).{8,}");
         String password = scanner.next();
         Matcher matcher = pattern.matcher(password);
-        if(matcher.matches()){
+        if (matcher.matches()) {
             System.out.println("Valid password");
-        }else
+        } else
             System.out.println("Invalid password");
 
     }
