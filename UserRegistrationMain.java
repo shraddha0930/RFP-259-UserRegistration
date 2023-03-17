6a582d62a70883f4d7106e685b88b80a0488ee7e
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-17 19:41:45 +0530

    Refactor:code to check all email sample provided seperately

--- UserRegistrationMain.java
+++ UserRegistrationMain.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class UserRegistrationMain {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to User Registration Problem");
         UserValidations userValidations = new UserValidations();
+        System.out.print("Enter the option: ");
+        int option = scanner.nextInt();
        // userValidations.username();
        // userValidations.useremail();
         // userValidations.mobilenumber();
-        userValidations.userpassword();
+        //userValidations.userpassword();
+        switch (option){
+            case 1:
+                userValidations.username();
+            case 2:
+                userValidations.useremail();
+            case 3:
+                userValidations.mobilenumber();
+            case 4:
+                userValidations.userpassword();
+        }
     }
 }
